592edad245296a0344529d72b45bc973437fd5f9
Nakornie <nakombre@gmail.com>
2024-10-11 17:58:54 +0200

    Debuted PHP version No change from HTML version yet

---
+++ PHP/Games/Morpion/morpion.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Jeu du Morpion</title>
+        <link href="../../style.css" rel="stylesheet">
+    </head>
+
+    <body>
+        <header>
+            <h1>Jeux du Morpion</h1>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="../../index.php">Accueil</a>
+                    </li>
+                    <li>
+                        <a href="#Règles_morpion">Règles</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+        
+        <main>
+            <img src="../../Images/morpion.jpeg" alt="Morpion" title="Bientôt utilisable" class="remplacement">
+            <h1 id="Règles_morpion" class="banniere">Règles du jeu</h1>
+            <h2 class="banniere">But du jeu</h2>
+            <h2 class="banniere">Déroulement du jeu</h2>
+        </main>
+
+        <footer>
+            <p>Pour toutes réclamations regardant la qualité du site... Essayez voir, je vous regarde : )</p>
+        </footer>
+    </body>
+</html>
---
+++ publish.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+cp -r ./PHP/ /Applications/XAMPP/xamppfiles/htdocs/webgames/
